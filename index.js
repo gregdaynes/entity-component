cf2967abc0255bcc8e0d613e58c1c19647a77412
Gregory Daynes <gregdaynes@gmail.com>
2021-08-21 15:42:59 -0700

    chore: Add system and example processTick loop
    Add accessor functions to get components and entities from the entity
    manager.

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ export class EntityManager {
     }
 
     // this could be a set!
-    if (!store[entityUUID].includes(component)) {
+    if (store[entityUUID] && !store[entityUUID].includes(component)) {
       store[entityUUID].push(component)
     } else {
       store[entityUUID] = [component]
@@ -52,7 +52,7 @@ export class EntityManager {
     if (!componentClass) throw new Error('componentClass must be specified')
 
     const store = this.componentStores[componentClass]
-    if (!store || store[entityUUID].length > 0) {
+    if (!store || !store[entityUUID].length > 0) {
       return false
     } else {
       return Object.keys(store).includes(entityUUID)
@@ -73,6 +73,24 @@ export class EntityManager {
       )
     }
   }
+
+  allEntitiesWithComponentOfType(componentClass) {
+    if (!componentClass) throw new Error('componentClass must be specified')
+
+    return Object.keys(this.componentStores[componentClass]) || []
+  }
+
+  componentOfType(entityUUID, componentClass) {
+    if (!entityUUID) throw new Error('UUID must be specified')
+    if (!componentClass) throw new Error('componentClass must be specified')
+
+    const store = this.componentStores[componentClass]
+    if (!store || !store[entityUUID]) {
+      return {}
+    } else {
+      return store[entityUUID]
+    }
+  }
 }
 
 export class Component {
@@ -90,13 +108,62 @@ export class Component {
   }
 }
 
+export class DemoComponent extends Component {
+  constructor(value) {
+    super()
+    this.value = value
+  }
+
+  grow(qty) {
+    this.value = this.value + Math.abs(qty)
+  }
+}
+
+export class System {
+  processTick() {
+    throw new Error('Systems must override processTick()')
+  }
+}
+
+export class DemoSystem extends System {
+  processTick(delta, entityManager) {
+    const demoEntities =
+      entityManager.allEntitiesWithComponentOfType('DemoComponent')
+
+    for (let entity of demoEntities) {
+      const demoComponent = entityManager.componentOfType(
+        entity,
+        'DemoComponent'
+      )
+
+      demoComponent[0].grow(1)
+    }
+  }
+}
+
 const entityManager = new EntityManager()
-const entity = entityManager.createBasicEntity()
+const player = entityManager.createTaggedEntity('player')
+const player2 = entityManager.createTaggedEntity('player')
+entityManager.addComponent(player, new DemoComponent(10))
+entityManager.addComponent(player2, new DemoComponent(5))
+
+const demoSystem = new DemoSystem()
+
+const delta = 1
+for (let i = 0; i < 2; i = i + delta) {
+  // systems go here
+  demoSystem.processTick(delta, entityManager)
+
+  console.log('---') // end of tick
+}
 
 console.log(
   JSON.stringify({
-    entity,
-    entityHasComponent: entityManager.hasComponentOfType(entity, 'test'),
-    myComponent: new Component(),
+    player,
+    entityManager,
+    entityHasComponent: entityManager.hasComponentOfType(
+      player,
+      'DemoComponent'
+    ),
   })
 )
